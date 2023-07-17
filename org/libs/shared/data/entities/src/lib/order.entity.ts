6fc6627d8e2c1ef676d64b3b78060808572d0ec3
TomiPBass <Pizik3@seznam.cz>
2023-07-17 16:37:38 +0200

    feat: order entity interface

--- org/libs/shared/data/entities/src/lib/order.entity.ts
+++ org/libs/shared/data/entities/src/lib/order.entity.ts
@@ -1,3 +1,17 @@
-export interface IOrder {
-    code: string;
+import { Optional } from 'sequelize';
+
+export interface IOrderOwnAttributes {
+	code: string;
+	messageForOwner: string;
 }
+export interface IOrderReferenceAttributes {
+	OrderDetails: any[];
+}
+
+export interface IOrderUniqueAttributes
+	extends Pick<IOrderOwnAttributes, 'code'> {}
+export interface IOrderCreationAttributes
+	extends Optional<Omit<IOrderOwnAttributes, 'code'>, 'messageForOwner'> {}
+export interface IOrderAttributes
+	extends IOrderOwnAttributes,
+		IOrderReferenceAttributes {}
