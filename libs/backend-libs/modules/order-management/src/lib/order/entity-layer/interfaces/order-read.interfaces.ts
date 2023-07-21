112c11ccb3973029fd5c28d1bba6ea452136eed0
TomiPBass <Pizik3@seznam.cz>
2023-07-21 15:01:17 +0200

    feat: order-read service

--- libs/backend-libs/modules/order-management/src/lib/order/entity-layer/interfaces/order-read.interfaces.ts
+++ libs/backend-libs/modules/order-management/src/lib/order/entity-layer/interfaces/order-read.interfaces.ts
@@ -1,10 +1,43 @@
 /**
  * IOrderFindOneParams
  * IOrderFindOneQuery
- * 
+ *
  * IOrderFindManyParams
  * IOrderFindManyQuery
- * 
+ *
  * IOrderFindFirstParams
  * IOrderFindFirstQuery
  */
+
+import { IOrderAttributes } from '@backend-demo/backend-libs/entities';
+import { Optional } from 'sequelize';
+
+export interface IOrderFindOneParams extends Pick<IOrderAttributes, 'code'> {}
+
+export interface IOrderFindOneQuery {
+	includeAccount: boolean;
+}
+export interface IOrderFindFirstParams
+	extends Optional<
+		Pick<IOrderAttributes, 'AccountCode' | 'messageForOwner'>,
+		'AccountCode' | 'messageForOwner'
+	> {}
+
+export interface IOrderFindFirstQuery {
+	includeAccount: boolean;
+}
+
+export interface IOrderFindManyParams
+	extends Optional<
+		Pick<IOrderAttributes, 'AccountCode' | 'messageForOwner'>,
+		'AccountCode' | 'messageForOwner'
+	> {}
+
+export interface IOrderFindManyQuery {
+	includeAccount: boolean;
+}
+
+export interface IOrderFindAndCountManyQuery extends IOrderFindManyQuery {
+	limit: number;
+	page: number;
+}
--- libs/backend-libs/modules/order-management/src/lib/order/entity-layer/order-read.service.ts
+++ libs/backend-libs/modules/order-management/src/lib/order/entity-layer/order-read.service.ts
@@ -1,9 +1,88 @@
 /**
  * orderRepository from orderProvider
- * 
+ *
  * findOne(params: IOrderFindOneParams, query?: IOrderFindOneQuery)
  * findFirst
  * findAll
  * findAndCountAll
  * count
  */
+
+import { Account, Order } from '@backend-demo/backend-libs/tables';
+import { Inject } from '@nestjs/common';
+import {
+	IOrderFindAndCountManyQuery,
+	IOrderFindFirstParams,
+	IOrderFindFirstQuery,
+	IOrderFindManyParams,
+	IOrderFindManyQuery,
+	IOrderFindOneParams,
+	IOrderFindOneQuery,
+} from './interfaces/order-read.interfaces';
+
+export class OrderReadService {
+	constructor(
+		@Inject('ORDER_REPOSITORY') private orderRepository: typeof Order
+	) {}
+
+	async findOne(
+		params: IOrderFindOneParams,
+		query?: IOrderFindOneQuery
+	): Promise<Order | null> {
+		return await this.orderRepository.findOne<Order>({
+			where: { code: params.code },
+			include: query?.includeAccount ? Account : undefined,
+		});
+	}
+
+	async findFirst(
+		params: IOrderFindFirstParams,
+		query?: IOrderFindFirstQuery
+	): Promise<Order | null> {
+		const orders = await this.orderRepository.findAll<Order>({
+			where: {
+				messageForOwner: params.messageForOwner,
+				AccountCode: params.AccountCode,
+			},
+			include: query?.includeAccount ? Account : undefined,
+		});
+		return orders[0];
+	}
+
+	async findAll(
+		params: IOrderFindManyParams,
+		query?: IOrderFindManyQuery
+	): Promise<Order[] | null> {
+		return await this.orderRepository.findAll<Order>({
+			where: {
+				messageForOwner: params.messageForOwner,
+				AccountCode: params.AccountCode,
+			},
+			include: query?.includeAccount ? Account : undefined,
+		});
+	}
+
+	async findAndCountAll(
+		params: IOrderFindManyParams,
+		query?: IOrderFindAndCountManyQuery
+	): Promise<Order[] | null> {
+		return await this.orderRepository.findAll<Order>({
+			where: {
+				messageForOwner: params.messageForOwner,
+				AccountCode: params.AccountCode,
+			},
+			include: query?.includeAccount ? Account : undefined,
+			limit: query?.limit ?? 10,
+			offset: query?.page ? query.page - 1 : 0,
+		});
+	}
+
+	async count(params: IOrderFindManyParams): Promise<number> {
+		return this.orderRepository.count<Order>({
+			where: {
+				messageForOwner: params.messageForOwner,
+				AccountCode: params.AccountCode,
+			},
+		});
+	}
+}
