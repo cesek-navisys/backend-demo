c0a1ab341abd46f677b02ede539da2847241a118
TomiPBass <Pizik3@seznam.cz>
2023-08-08 11:31:36 +0200

    fix: merge bug fix

--- libs/backend-libs/modules/order-management/src/lib/order-management-query.service.ts
+++ libs/backend-libs/modules/order-management/src/lib/order-management-query.service.ts
@@ -1,13 +1,19 @@
-import {
-	FindProductByCodeQuery,
-	IFindProductByCodeQuery,
-} from '@backend-demo/backend-libs/queries';
 import { Injectable } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
+import {
+	FindAccountByCodeQuery,
+	FindProductByCodeQuery,
+	IFindAccountByCodeQuery,
+	IFindProductByCodeQuery,
+} from '@backend-demo/backend-libs/queries';
 
 @Injectable()
 export class OrderManagementQueryService {
 	constructor(private readonly queryBus: QueryBus) {}
+
+	async queryAccountByCode(params: IFindAccountByCodeQuery) {
+		return this.queryBus.execute(new FindAccountByCodeQuery(params));
+	}
 
 	async queryProductByCode(params: IFindProductByCodeQuery) {
 		return this.queryBus.execute(new FindProductByCodeQuery(params));
