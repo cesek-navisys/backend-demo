bf82174c6a93cd0f9eacbd2676a63a193eff64df
Doberman786 <sasha19822010@gmail.com>
2023-08-10 16:48:56 +0200

    feat: Added OrderDetails to the ProductViewMapper

--- libs/backend-libs/modules/product-management/src/lib/product/dto/view-product-mapper.dto.ts
+++ libs/backend-libs/modules/product-management/src/lib/product/dto/view-product-mapper.dto.ts
@@ -3,15 +3,19 @@ import { Expose, Type } from 'class-transformer';
 import { IProductViewMapper } from './interfaces/view-product-mapper.interface';
 import { ViewAccountDto } from '@backend-demo/backend-libs/modules/account-management';
 import { ViewProductDto } from './view-product.dto';
+import { ViewOrderDetailsDto } from 'libs/backend-libs/modules/order-management/src/lib/order-details/dto';
 
 export class ViewProductMapperDto
 	extends ViewProductDto
 	implements IProductViewMapper
 {
-	@ApiPropertyOptional({
-		type: ViewAccountDto,
-	})
+	@ApiPropertyOptional()
 	@Type(() => ViewAccountDto)
 	@Expose()
 	Account?: ViewAccountDto;
+
+	@ApiPropertyOptional()
+	@Type(() => ViewOrderDetailsDto)
+	@Expose()
+	OrderDetails?: ViewOrderDetailsDto;
 }
